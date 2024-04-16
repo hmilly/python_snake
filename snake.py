a7296e83c8f936f72228c759264dca5c7a36fa04
Holly <milstowers@gmail.com>
2024-04-16 04:28:29 +0100

    adding functionality to game / halfway to working

--- snake.py
+++ snake.py
@@ -43,15 +43,51 @@ win_y = int((screen_h / 2) - (win_h / 2))
 
 window.geometry(f"{win_w}x{win_h}+{win_x}+{win_y}")
 
+
 # initialise game
 # snakes head
 snake = Tile(5 * TILE_SIZE, 5 * TILE_SIZE)
 food = Tile(10 * TILE_SIZE, 15 * TILE_SIZE)
+snake_body = []
 velX = 0
 velY = 0
 
+
+def change_dir(e):
+    global velX, velY
+
+    if e.keysym == "Up" and velY != 1:
+        velX = 0
+        velY = -1
+    elif e.keysym == "Down" and velY != -1:
+        velX = 0
+        velY = 1
+    elif e.keysym == "Left" and velX != 1:
+        velX = -1
+        velY = 0
+    elif e.keysym == "Right" and velX != -1:
+        velX = 1
+        velY = 0
+
+
+def move():
+    global snake
+
+    # collision
+    if snake.x == food.x and snake.y == food.y:
+        snake_body.append(Tile(food.x, food.y))
+        food.x = random.randint(0, COLS - 1) * TILE_SIZE
+        food.y = random.randint(0, ROWS - 1) * TILE_SIZE
+
+    snake.x += velX * TILE_SIZE
+    snake.y += velY * TILE_SIZE
+
+
 def draw():
     global snake
+    move()
+    canvas.delete("all")
+
     #  draw food
     canvas.create_rectangle(
         food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill="blue"
@@ -61,8 +97,15 @@ def draw():
         snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill="#e619a2"
     )
 
+    for tile in snake_body:
+        canvas.create_rectangle(
+            tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill="#e619a2"
+        )
+
     window.after(100, draw)
     # 10 fps
 
 
 draw()
+window.bind("<KeyRelease>", change_dir)
+window.mainloop()
