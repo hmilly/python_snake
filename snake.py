6dc7cfc522c7408093a694233285ff74c6dcb7dc
Holly <milstowers@gmail.com>
2024-04-17 04:31:06 +0100

    finished functionality - game over, score etc

--- snake.py
+++ snake.py
@@ -20,7 +20,6 @@ window = tkinter.Tk()
 window.title("Snake")
 window.resizable = (False, False)
 
-
 canvas = tkinter.Canvas(
     window,
     bg="black",
@@ -51,10 +50,14 @@ food = Tile(10 * TILE_SIZE, 15 * TILE_SIZE)
 snake_body = []
 velX = 0
 velY = 0
+game_over = False
+score = 0
 
 
 def change_dir(e):
     global velX, velY
+    if game_over:
+        return
 
     if e.keysym == "Up" and velY != 1:
         velX = 0
@@ -71,20 +74,48 @@ def change_dir(e):
 
 
 def move():
-    global snake
+    global snake, food, snake_body, score, game_over
+    if game_over:
+        return
+
+    if (
+        snake.x < 0
+        or snake.x >= WINDOW_WIDTH
+        or snake.y < 0
+        or snake.y >= WINDOW_HEIGHT
+    ):
+        game_over = True
+        return
+
+    for tile in snake_body:
+        if snake.x == tile.x and snake.y == tile.y:
+            game_over = True
+            return
 
     # collision
     if snake.x == food.x and snake.y == food.y:
         snake_body.append(Tile(food.x, food.y))
         food.x = random.randint(0, COLS - 1) * TILE_SIZE
         food.y = random.randint(0, ROWS - 1) * TILE_SIZE
+        score += 1
+
+    # update snakes body
+    for i in range(len(snake_body) - 1, -1, -1):
+        tile = snake_body[i]
+        if i == 0:
+            tile.x = snake.x
+            tile.y = snake.y
+        else:
+            prev_tile = snake_body[i - 1]
+            tile.x = prev_tile.x
+            tile.y = prev_tile.y
 
     snake.x += velX * TILE_SIZE
     snake.y += velY * TILE_SIZE
 
 
 def draw():
-    global snake
+    global snake, food, snake_body, game_over, score
     move()
     canvas.delete("all")
 
@@ -100,6 +131,26 @@ def draw():
     for tile in snake_body:
         canvas.create_rectangle(
             tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill="#e619a2"
+        )
+
+    if game_over:
+        canvas.create_text(
+            WINDOW_WIDTH / 2,
+            WINDOW_HEIGHT / 2,
+            font="Arial 20",
+            text=f"Game Over!",
+            fill="white",
+        )
+        canvas.create_text(
+            WINDOW_WIDTH / 2,
+            WINDOW_HEIGHT / 2 + 35,
+            font="Arial 20",
+            text=f"Score: {score}",
+            fill="white",
+        )
+    else:
+        canvas.create_text(
+            30, 20, font="Arial 10", text=f"Score: {score}", fill="white"
         )
 
     window.after(100, draw)
